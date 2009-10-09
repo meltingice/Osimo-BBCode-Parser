bbd8d0f2925b164d71f7dde76454497597e4e755
meltingice <meltingice8917@gmail.com>
2009-10-09 08:37:33 -0700

    Added a very flexible [table] tag that can be customized with CSS, also some regex improvements.

--- bbparser.php
+++ bbparser.php
@@ -3,6 +3,7 @@ class OsimoBBParser{
 	private $isOsimo;
 	private $simple_bbcodes;
 	private $fancy_bbcodes;
+	private $allowedCSS;
 	
 	function OsimoBBParser(){
 		$this->isOsimo = true;
@@ -13,92 +14,124 @@ class OsimoBBParser{
 	private function loadDefaults(){
 		$simple_defaults = array(
 			"b"=>array(
-				"search"=>"/\[b\](.+)\[\/b\]/i",
+				"search"=>"/\[b\](.+?)\[\/b\]/is",
 				"replace"=>'<strong>$1</strong>'
 			),
 			"i"=>array(
-				"search"=>"/\[i\](.+)\[\/i\]/i",
+				"search"=>"/\[i\](.+?)\[\/i\]/is",
 				"replace"=>'<i>$1</i>'
 			),
 			"u"=>array(
-				"search"=>"/\[u\](.+)\[\/u\]/i",
+				"search"=>"/\[u\](.+?)\[\/u\]/is",
 				"replace"=>'<u>$1</u>'
 			),
 			"s"=>array(
-				'search'=>"/\[s\](.+)\[\/s\]/i",
+				'search'=>"/\[s\](.+?)\[\/s\]/is",
 				'replace'=>'<span style="text-decoration:line-through">$1</span>'
 			),
 			"url"=>array(
-				"search"=>"/\[url\](.+)\[\/url\]/i",
+				"search"=>"/\[url\](.+?)\[\/url\]/is",
 				"replace"=>'<a href="$1">$1</a>'
 			),
 			"img"=>array(
-				"search"=>"/\[img\](.+)\[\/img\]/i",
+				"search"=>"/\[img\](.+?)\[\/img\]/is",
 				"replace"=>'<img src="$1" />'
 			),
 			"quote"=>array(
-				"search"=>"/\[quote\](.+)\[\/quote\]/i",
+				"search"=>"/\[quote\](.+?)\[\/quote\]/is",
 				"replace"=>'<blockquote><span class="blockquote-title">Quote:</span><br/>$1</blockquote>'
 			),
 			'code'=>array(
-				'search'=>"/\[code\](.+)\[\/code\]/i",
+				'search'=>"/\[code\](.+?)\[\/code\]/is",
 				'replace'=>'<pre class="code"><code class="plain">$1</code></pre>'
 			),
 			'right'=>array(
-				'search'=>"/\[right\](.+)\[\/right\]/i",
+				'search'=>"/\[right\](.+?)\[\/right\]/is",
 				'replace'=>'<div style="text-align:right">$1</div>'
 			),
 			'center'=>array(
-				'search'=>"/\[center\](.+)\[\/center\]/i",
+				'search'=>"/\[center\](.+?)\[\/center\]/is",
 				'replace'=>'<div style="text-align:center">$1</div>'
 			),
 			'left'=>array(
-				'search'=>"/\[left\](.+)\[\/left\]/i",
+				'search'=>"/\[left\](.+?)\[\/left\]/is",
 				'replace'=>'<div style="text-align:left">$1</div>'
 			),
 			'list'=>array(
-				'search'=>"/\[list\](.+)\[\/list\]/is",
+				'search'=>"/\[list\](.+?)\[\/list\]/is",
 				'replace'=>'<ul>$1</ul>'
 			),
 			'[*]'=>array(
-				'search'=>"/\[\*\]([^\n|\r]+)/i",
+				'search'=>"/\[\*\]([^\n|\r]+)/is",
 				'replace'=>'<li>$1</li>'
+			),
+			'table'=>array(
+				'search'=>"/\[table\](.+?)\[\/table\]/is",
+				'replace'=>'<table>$1</table>'
+			),
+			'row'=>array(
+				'search'=>"/\[row\](.+?)\[\/row\]/is",
+				'replace'=>'<tr>$1</tr>'
+			),
+			'cell'=>array(
+				'search'=>"/\[cell\](.+?)\[\/cell\]/is",
+				'replace'=>'<td>$1</td>'
 			)
 		);
 		
 		$fancy_defaults = array(
 			'email'=>array(
-				'search'=>"/\[email=([^\]]+)\](.+)\[\/email\]/i",
+				'search'=>"/\[email=([^\]]+)\](.+?)\[\/email\]/is",
 				'replace'=>'<a href="$1">$2</a>'
 			),
 			"url"=>array(
-				"search"=>"/\[url=([^\]]+)\](.+)\[\/url\]/i",
+				"search"=>"/\[url=([^\]]+)\](.+?)\[\/url\]/is",
 				"replace"=>'<a href="$1">$2</a>'
 			),
 			'size'=>array(
-				'search'=>"/\[size=([0-9]+)\](.+)\[\/size\]/i",
+				'search'=>"/\[size=([0-9]+)\](.+?)\[\/size\]/is",
 				'replace'=>'<span style="font-size:$1px">$2</span>'
 			),
 			'font'=>array(
-				'search'=>"/\[font=([^\]]+)\](.+)\[\/font\]/i",
+				'search'=>"/\[font=([^\]]+)\](.+?)\[\/font\]/is",
 				'replace'=>'<span style="font-family:$1">$2</span>'
 			),
 			'color'=>array(
-				'search'=>"/\[color=([^\]]+)\](.+)\[\/color\]/i",
+				'search'=>"/\[color=([^\]]+)\](.+?)\[\/color\]/is",
 				'replace'=>'<span style="color:$1">$2</span>'
 			),
 			'quote'=>array(
-				'search'=>"/\[quote=([^\]]+)\](.+)\[\/quote\]/i",
+				'search'=>"/\[quote=([^\]]+)\](.+?)\[\/quote\]/is",
 				'replace'=>'<blockquote><span class="blockquote-title">Quote by $1:</span><br/>$2</blockquote>'
 			),
 			'align'=>array(
-				'search'=>"/\[align=(left|right|center)\](.+)\[\/align\]/i",
+				'search'=>"/\[align=(left|right|center)\](.+?)\[\/align\]/is",
 				'replace'=>'<div style="text-align: $1">$2</div>'
 			)
 		);
 		
 		$this->addSimple($simple_defaults);
 		$this->addFancy($fancy_defaults);
+		
+		/* Allowed CSS attributes for tables - prevents things like
+		 * using position: absolute to cover the page contents. */
+		$this->allowedCSS = array(
+			'width',
+			'height',
+			'background-color',
+			'color',
+			'border',
+			'border-color',
+			'border-style',
+			'border-width',
+			'border-collapse',
+			'padding',
+			'vertical-align',
+			/* these next 2 aren't CSS but are useful for tables */
+			'cellpadding',
+			'cellspacing',
+			'align' // not a real CSS property - will allow for easy alignment
+		);
 	}
 	
 	public function addSimple($tag){
@@ -110,7 +143,7 @@ class OsimoBBParser{
 		else{ //make an assumption...
 			$this->simple_bbcodes[$tag] = array(
 				"tag"=>$tag,
-				"search"=>"/\[$tag\](.+)\[\/$tag\]/gi",
+				"search"=>"/\[$tag\](.+)\[\/$tag\]/i",
 				"replace"=>'<'.$tag.'>$1</'.$tag.'>'
 			);
 		}
@@ -141,9 +174,13 @@ class OsimoBBParser{
 			$replace[] = $code['replace'];
 		}
 		
+		if(get_magic_quotes_gpc()){
+			$content = stripslashes($content);
+		}
+		
 		/* nocode tag check - must do this first! */
 		$content =  preg_replace_callback(
-			'/\[nocode\](.+)\[\/nocode\]/i',
+			'/\[nocode\](.+?)\[\/nocode\]/is',
 			create_function(
 				'$matches',
 				'return str_replace(array("[","]"),array("&#91;","&#93;"),$matches[1]);'
@@ -151,7 +188,72 @@ class OsimoBBParser{
 			$content
 		);
 		
-		return nl2br(preg_replace($search,$replace,$content,-1,$count));
+		/* unfortunately we have to do fancy tables separately */
+		$content = preg_replace_callback(
+			"/\[(table)=([^\]]+)\](.+?)\[\/table\]/is",
+			array($this,'parseTable'),
+			$content
+		);
+		$content = preg_replace_callback(
+			"/\[(row)=([^\]]+)\](.+?)\[\/row\]/is",
+			array($this,'parseTable'),
+			$content
+		);
+		$content = preg_replace_callback(
+			"/\[(cell)=([^\]]+)\](.+?)\[\/cell\]/is",
+			array($this,'parseTable'),
+			$content
+		);
+		
+		foreach($search as $pattern){
+			while(preg_match($pattern,$content)){
+				$content = preg_replace($search,$replace,$content,-1,$count);
+			}
+		}
+		
+		return $content;
+	}
+	
+	private function parseTable($matches){
+		$return_css = '';
+		$return_attr = '';
+		$css = explode(';',$matches[2]);
+		if(count($css)>0){
+			foreach($css as $prop){
+				$data = explode(':',$prop);
+				$css_name = strtolower($data[0]);
+				if(in_array($css_name,$this->allowedCSS)){
+					if($matches[1]=='table'){
+						if($css_name == 'cellpadding'||$css_name == 'cellspacing'){
+							$return_attr .= $css_name.'="'.$data[1].'" ';
+							continue;
+						}
+						
+						if($css_name == 'align'){
+							if($data[1] == 'left'){
+								$return_css .= 'margin-right: auto;';
+							}
+							elseif($data[1] == 'center'){
+								$return_css .= 'margin-right: auto;margin-left: auto;';
+							}
+							elseif($data[1] == 'right'){
+								$return_css  .= 'margin-left: auto;';
+							}
+							continue;
+						}
+					}
+					
+					$return_css .= $prop;
+					if(substr($prop,strlen($prop)-1) != ';'){
+						$return_css .= ';';
+					}
+				}
+			}
+		}
+		if($matches[1]=='row'){ $ele_name = 'tr'; }
+		elseif($matches[1]=='cell'){ $ele_name = 'td'; }
+		else{ $ele_name = 'table'; }
+		return "<$ele_name $return_attr style=\"$return_css\">{$matches[3]}</$ele_name>";
 	}
 }
 ?>
